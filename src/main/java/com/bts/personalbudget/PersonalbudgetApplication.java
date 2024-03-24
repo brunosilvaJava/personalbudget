038137acd4f647ebb368bc6cd5815e064d709743
Bruno Silva <bruno.silva890@etec.sp.gov.br>
2024-03-24 13:42:12 -0300

    first setup project

---
+++ src/main/java/com/bts/personalbudget/PersonalbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.bts.personalbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalbudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bts/personalbudget/PersonalbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bts.personalbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
