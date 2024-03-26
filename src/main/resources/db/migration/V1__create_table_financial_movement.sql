398c8a674fc0f6a9147a98665088351e8c3969d1
Bruno Silva <bruno.silva890@etec.sp.gov.br>
2024-03-26 14:58:24 -0300

    BTS-002 - create financial_movement table

---
+++ src/main/resources/db/migration/V1__create_table_financial_movement.sql
@@ -0,0 +1,10 @@
+create table FINANCIAL_MOVEMENT(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT COMMENT 'chave primária',
+    operation_type VARCHAR(20) NOT NULL COMMENT 'tipo da operação financeira (CREDIT; DEBIT)',
+    description VARCHAR(50) NOT NULL COMMENT 'descrição da movimentação financeira',
+    amount DECIMAL(4, 2) NOT NULL COMMENT 'valor da movimentação financeira',
+    amount_paid DECIMAL(4,2) COMMENT 'valor do pagamento',
+    movement_date DATETIME NOT NULL COMMENT 'data da movimentação financeira',
+    status VARCHAR(20) NOT NULL COMMENT 'situação da movimentação financeira',
+    flag_active TINYINT(1) NOT NULL COMMENT 'flag de esclusão lógica'
+)
