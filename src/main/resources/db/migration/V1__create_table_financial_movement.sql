c6fcc1163e02ff4a95de0c321def8428623b2717
Eduardo Henrick <eduardohenricksouza@gmail.com>
2024-03-26 19:42:45 -0300

    BTS-002 ajuste no migration

--- src/main/resources/db/migration/V1__create_table_financial_movement.sql
+++ src/main/resources/db/migration/V1__create_table_financial_movement.sql
@@ -5,6 +5,7 @@ create table FINANCIAL_MOVEMENT(
     amount DECIMAL(4, 2) NOT NULL COMMENT 'valor da movimentação financeira',
     amount_paid DECIMAL(4,2) COMMENT 'valor do pagamento',
     movement_date DATETIME NOT NULL COMMENT 'data da movimentação financeira',
+    due_date DATETIME NOT NULL COMMENT 'data do vencimento da movimentação financeira',
+    pay_date DATETIME COMMENT 'data do pagamento da movimentação financeira',
     status VARCHAR(20) NOT NULL COMMENT 'situação da movimentação financeira',
-    flag_active TINYINT(1) NOT NULL COMMENT 'flag de esclusão lógica'
-)
+    flag_active TINYINT(1) NOT NULL COMMENT 'flag de exclusão lógica'
